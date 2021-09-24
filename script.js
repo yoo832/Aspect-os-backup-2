8ebf536e4d5e8e8027eec0e81c3b36c1ba2c3cc2
Glitch (glitch-blank-website) <none>
2021-09-24 14:51:50 +0000

    🏵🍩 Checkpoint ./script.js:36284934/439 ./style.css:36284934/1269 ./index.html:36284934/2766

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
 console.log("Aspect");
 
-let person = prompt("Please enter your scratch username", "John Doe");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-} else {
-  text = "Hello " + person + "! How are you today?";
-}
-alert("Welcome + pe");
